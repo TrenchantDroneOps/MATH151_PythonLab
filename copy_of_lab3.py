e6c16a378a3150495df6a350815a0cee880662eb
SamMartinez1 <113560260+SamMartinez1@users.noreply.github.com>
2022-09-17 20:21:14 -0500

    Update copy_of_lab3.py

--- copy_of_lab3.py
+++ copy_of_lab3.py
@@ -40,21 +40,40 @@ print('the root of the function inside [0,1] is', nsolve(f,x,0))
 #### 2a
 """
 
-f1 = 2*x-3
-f2 = 4*x-x**2
-f3 = (x**2-6*x+8)/(x-4)
-f4 = exp(((x-4)*(log(3))))
+f1 = 2*x - 3
+f2 = 4*x - x**2
+f3 = (x**2 - 6*x + 8)/(x-4)
+f4 = exp((x-4) * log(3))
+fo3_lhl = limit(f1,x,3)
+fo3_rhl = limit(f2,x,3)
+fo4_lhl = limit(f2,x,4)
+fo4_rhl = limit(f3,x,4)
+fo5_lhl = limit(f3,x,5)
+fo5_rhl = limit(f4,x,5)
 
-fo3_1h1 = limit(f1,x,3)
-fo3_rh1 = limit(f2, x, 3)
+print('As x approaches 3 from the left, the limit of f(x) approaches', fo3_lhl)
+print('As x approaches 3 from the right, the limit of f(x) approaches', fo3_rhl)
+print('Since both lhl and rhl are 3, f is continuous at x=3')
 
+#e^(stuff) is written exp(stuff)
+#ln(stuff) is written log(stuff)
+#infinity is written oo (double letter o)
+#only graph question 3 up to 100, not infinity
 
-print("lim of f(1) from left", fo3_1h1)
-print("lim of f(1) from right", fo3_rh1)
+print()
+print('As x approaches 4 from the left, the limit of f(x) approaches', fo4_lhl)
+print('As x approaches 4 from the right, the limit of f(x) approaches', fo4_rhl)
+print('Since the lhl and rhl are not equal, the limit of f(x) at x=4 does not exist')
+print('f is continuous from the left at x=4')
+
+print()
+print('As x approaches 5 from the left, the limit of f(x) approaches', fo5_lhl)
+print('As x approaches 5 from the right, the limit of f(x) approaches', fo5_rhl)
+print('Since both lhl and rhl are 3, f is continuous at x=5')
+
 
 """#### 2b"""
-
-plot( 2*x-3,4*x-x**2,(x**2-6*x+8)/(x-4),exp(((x-4)*(log(3))))   )
+plot((f1,(x,0,3)),(f2, (x,3,4)), (f3, (x,4,5)), (f4, (x,5,6)))
 
 """### Question 3
 
